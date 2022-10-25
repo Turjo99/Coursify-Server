d608d35f3aa38980b1930262ca7e8f8df47ea677
Turjo99 <imturjo27@gmail.com>
2022-10-25 20:22:35 +0600

    Dynamically Data Loading Done

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
+
+app.get("/", (req, res) => {
+  res.send("courses API Running");
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+
+  const selectedCategory = courses.filter((n) => n.category_id === id);
+  res.send(selectedCategory);
+});
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedcourses = courses.filter((n) => n._id == id);
+  res.send(selectedcourses);
+});
+
+app.listen(port, () => {
+  console.log("courses Server running on port", port);
+});
